8fa8f50d5d22ae22569c5b819de934893a1bac9f
Тарасов Артур Владимирович <D:\Mail the bat!>
2025-09-15 15:39:30 +0300

    feat:burger menu btn

--- js/script.js
+++ js/script.js
@@ -1,2 +1,7 @@
-const screenY = document.documentElement.clientHeight
-console.log(screenY)
+const burger = document.querySelector('.burger-menu');
+const nav = document.querySelector('nav');
+
+burger.addEventListener('click', () => {
+  burger.classList.toggle('active');
+  nav.classList.toggle('active');
+});

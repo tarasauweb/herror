63f3395afee963464d55ea5ace967305461be5da
Тарасов Артур Владимирович <D:\Mail the bat!>
2025-09-15 16:08:52 +0300

    feat:burger menu + functional

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
 const burger = document.querySelector('.burger-menu');
-const nav = document.querySelector('nav');
-
+const overlay = document.querySelector('.overlay');
+const menu = document.querySelector('.menu');
+const offer = document.querySelector('.offer');
 burger.addEventListener('click', () => {
   burger.classList.toggle('active');
-  nav.classList.toggle('active');
+  overlay.classList.toggle('active');
+  menu.classList.toggle('active');
+  offer.classList.toggle('active');
 });

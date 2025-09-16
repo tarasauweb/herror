d70732a4b2377d8cdcd6a9c72b35c76e57b46535
Тарасов Артур Владимирович <D:\Mail the bat!>
2025-09-16 10:31:48 +0300

    fix:scroll burger menu

--- js/script.js
+++ js/script.js
@@ -2,9 +2,11 @@ const burger = document.querySelector('.burger-menu');
 const overlay = document.querySelector('.overlay');
 const menu = document.querySelector('.menu');
 const offer = document.querySelector('.offer');
+const body = document.querySelector('body');
 burger.addEventListener('click', () => {
   burger.classList.toggle('active');
   overlay.classList.toggle('active');
   menu.classList.toggle('active');
   offer.classList.toggle('active');
+  body.classList.toggle('active');
 });
